802b4384c008e30c9753effae217b39aa79dc2d4
Jaakko Rinta-Filppula <jaakko.rf@gmail.com>
2015-08-18 16:39:03 +0300

    remove unused test code

--- index.js
+++ index.js
@@ -14,25 +14,3 @@ juvenes.getMenus(today, function(menus) {
 sodexo.getMenus(today, function(menus) {
     console.log(menus[0].meals[1]);
 });
-
-
-var result = '';
-
-var req = http.get('http://www.juvenes.fi/DesktopModules/Talents.LunchMenu/LunchMenuServices.asmx/GetMenuByWeekday?KitchenId=6&MenuTypeId=60&Week=34&Weekday=1&lang=%27fi%27&format=json',
-    function(res) {
-        res.on('data', function(data) {
-            result = result + data.toString();
-        });
-    });
-    
-req.on('close', function() {
-    result = result.slice(1).slice(0, -2);
-    
-    var data = JSON.parse(JSON.parse(result).d);
-    // console.dir(data);
-    
-});
-    
-req.on('error', function(err) {
-    // console.error(err);
-});
