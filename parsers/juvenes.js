8fe8e6518e4aacc50dc9acc248ee0f88a3b5ad25
Jaakko Rinta-Filppula <jaakko.rf@gmail.com>
2015-08-25 20:09:23 +0300

    parse some opening hours

--- parsers/juvenes.js
+++ parsers/juvenes.js
@@ -1,5 +1,6 @@
 var http = require('http'),
-    async = require('async');
+    async = require('async'),
+    entities = new (require('html-entities').AllHtmlEntities)();
 
 var restaurants = [
     {
@@ -20,7 +21,70 @@ var restaurants = [
     }
 ];
 
-var baseUrl = 'http://www.juvenes.fi/DesktopModules/Talents.LunchMenu/LunchMenuServices.asmx/GetMenuByWeekday?';
+var baseUrl = 'http://www.juvenes.fi/DesktopModules/Talents.LunchMenu/LunchMenuServices.asmx/GetMenuByWeekday?',
+    infoUrl = 'http://www.juvenes.fi/DesktopModules/Talents.LunchMenu/LunchMenuServices.asmx/GetKitchenInfo?';
+    
+exports.getOpeningHours = function(callback) {
+    var options = {
+        KitchenInfoId: 2352,
+        lang: "'fi'",
+        format: 'json'
+    };
+    
+    var queryString = Object.keys(options).map(function(key) {
+        return key + '=' + options[key];
+    }).join('&');
+    
+    var result = '';
+    var req = http.get(infoUrl + queryString, function(res) {
+        res.on('data', function(data) {
+            result += data.toString();
+        });
+    });
+    
+    req.on('close', function() {
+        result = result.slice(1).slice(0, -2);
+        
+        var resultObj = JSON.parse(result);
+        if (resultObj.d) {
+            resultObj.d = resultObj.d.replace(/\\u000a/g, '\n').replace(/<br( \\\/)?>/g, '').replace(/\\/g, '');
+            resultObj.d = entities.decode(resultObj.d);
+            
+            var trs = resultObj.d.split('</tr>').slice(1).slice(0, -1);
+            trs = trs.map(function(tr) {
+                return tr.replace('<tr>', '').split('</td>').map(function(td) {
+                    return td.trim().replace('<td>', '').replace(/^\s+/g, '').replace(/ {2,}/g, '')
+                        .split(/\n/)
+                        .map(function(item) {
+                            return item.replace(/<.+?>/g, '').trim();
+                        })
+                        .filter(function(item) {
+                            return item !== '';
+                        });
+                }).filter(function(td) {
+                    return td.length > 0;
+                });
+            });
+            
+            var infos = trs.map(function(tr) {
+                return {
+                    menu: tr[0].slice(0, tr[0].length - tr[1].length > 0 ? tr[0].length - tr[1].length : tr[0].length).join(' '),
+                    hours: tr[2].map(function(time, index) {
+                        var diff = tr[0].length - tr[1].length,
+                            dayOptions = tr[0].slice(diff);
+                        
+                        return {
+                            time: time,
+                            days: diff > 0 ? dayOptions[index] : ''
+                        };
+                    })
+                };
+            });
+            
+            callback(infos);
+        }
+    });
+};
 
 exports.getMenus = function(date, callback) {
     var week = date.week(),
